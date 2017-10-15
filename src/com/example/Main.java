ebf60beed86dc2c9ea4ecf6de36e4dbe3d548c76
Qsior <lukaszbojes@gmail.com>
2017-10-15 17:21:51 +0200

    Copied code from previous project

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,23 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
+        }
     }
 }
