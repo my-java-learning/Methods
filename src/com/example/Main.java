e51588f216f34b30df0dc406489e60ed8f03fcca
Qsior <lukaszbojes@gmail.com>
2017-10-16 19:28:13 +0200

    Added calculateScore method, which now contains redundant blocks of code from main method. We can now call this new method with appropriate parameters in main.

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -8,18 +8,22 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was: " + finalScore);
-        }
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was: " + finalScore);
+            return finalScore;
         }
+        return -1;
     }
 }
