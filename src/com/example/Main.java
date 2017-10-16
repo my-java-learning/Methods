8a5e8c08dce7e4efe63654035f539f6f40360962
Qsior <lukaszbojes@gmail.com>
2017-10-16 20:45:10 +0200

    Added exercise

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -11,12 +11,14 @@ public class Main {
         int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
         System.out.println(highScore);
 
-        score = 10000;
+        score = 50;
         levelCompleted = 8;
         bonus = 200;
 
         highScore = calculateScore(gameOver, score, levelCompleted, bonus);
         System.out.println(highScore);
+
+        displayHighScorePosition("player1", score);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
@@ -25,5 +27,17 @@ public class Main {
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int score){
+        int printScore = calculateHighScorePosition(score);
+        System.out.println(playerName + " managed to get into position " + printScore + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if (score > 1000) return 1;
+        if (score > 500 && score < 1000) return 2;
+        if (score > 100 && score < 500) return 3;
+        else return 4;
     }
 }
